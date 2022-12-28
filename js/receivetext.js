54803ec13d527f00ff21ece6856019abcff0e4c0
thangnm89 <106656777+thangnm89@users.noreply.github.com>
2022-12-28 14:22:02 +0700

    Update receivetext.js

--- js/receivetext.js
+++ js/receivetext.js
@@ -1,48 +1,80 @@
 var TextReceiver = (function() {
-    Quiet.init({
-        profilesPrefix: "/",
-        memoryInitializerPrefix: "/",
-        libfecPrefix: "/"
-    });
-    var target;
-    var content = new ArrayBuffer(0);
-    // var warningbox;
+    var receivers;
+    
 
-    function onReceive(recvPayload) {
-        content = Quiet.mergeab(content, recvPayload);
-        target.textContent = Quiet.ab2str(content);
-        // warningbox.classList.add("hidden");
+    function onReceive(recvPayload, recvObj) {
+        recvObj.content = Quiet.mergeab(recvObj.content, recvPayload);
+        recvObj.target.textContent = Quiet.ab2str(recvObj.content);
+        recvObj.successes++;
+        var total = recvObj.failures + recvObj.successes
+        var ratio = recvObj.failures/total * 100;
+        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
     };
 
-    function onReceiverCreateFail(reason) {
+    function onReceiverCreateFail(reason, recvObj) {
         console.log("failed to create quiet receiver: " + reason);
-        // warningbox.classList.remove("hidden");
-        // warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
+        recvObj.warningbox.classList.remove("hidden");
+        recvObj.warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
     };
 
-    function onReceiveFail(num_fails) {
-        // warningbox.classList.remove("hidden");
-        // warningbox.textContent = "We didn't quite get that. It looks like you tried to transmit something. You may need to move the transmitter closer to the receiver and set the volume to 50%."
+    function onReceiveFail(num_fails, recvObj) {
+        recvObj.warningbox.classList.remove("hidden");
+        recvObj.failures = num_fails;
+        var total = recvObj.failures + recvObj.successes
+        var ratio = recvObj.failures/total * 100;
+        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
+    };
+
+    function onClick(e, recvObj) {
+        e.target.disabled = true;
+        var originalText = e.target.innerText;
+        e.target.innerText = e.target.getAttribute('data-quiet-receiving-text');
+        e.target.setAttribute('data-quiet-receiving-text', originalText);
+
+        var receiverOnReceive = function(payload) { onReceive(payload, recvObj); };
+        var receiverOnReceiverCreateFail = function(reason) { onReceiverCreateFail(reason, recvObj); };
+        var receiverOnReceiveFail = function(num_fails) { onReceiveFail(num_fails, recvObj); };
+        Quiet.receiver({profile: recvObj.profilename,
+            onReceive: receiverOnReceive,
+            onCreateFail: receiverOnReceiverCreateFail,
+            onReceiveFail: receiverOnReceiveFail
+        });
+
+        recvObj.target.classList.remove('hidden');
+    }
+
+    function setupReceiver(receiver) {
+        var recvObj = {
+            profilename: receiver.getAttribute('data-quiet-profile-name'),
+            btn: receiver.querySelector('[data-quiet-receive-text-button]'),
+            target: receiver.querySelector('[data-quiet-receive-text-target]'),
+            warningbox: receiver.querySelector('[data-quiet-receive-text-warning]'),
+            successes: 0,
+            failures: 0,
+            content: new ArrayBuffer(0),
+            
+        };
+       
+        var onBtnClick = function(e) { return onClick(e, recvObj); };
+        recvObj.btn.addEventListener('click', onBtnClick, false);
     };
 
     function onQuietReady() {
-        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
-        Quiet.receiver({profile: profilename,
-             onReceive: onReceive,
-             onCreateFail: onReceiverCreateFail,
-             onReceiveFail: onReceiveFail
-        });
+        for (var i = 0; i < receivers.length; i++) {
+            setupReceiver(receivers[i]);
+        }
     };
 
     function onQuietFail(reason) {
         console.log("quiet failed to initialize: " + reason);
-        // warningbox.classList.remove("hidden");
-        // warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
+        var warningbox = document.querySelector('[data-quiet-receive-text-warning]');
+        warningbox.classList.remove("hidden");
+        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
     };
 
     function onDOMLoad() {
-        target = document.querySelector('[data-quiet-receive-text-target]');
-        // warningbox = document.querySelector('[data-quiet-warning]');
+        receivers = document.querySelectorAll('[data-quiet-receive-text]');
+       
         Quiet.addReadyCallback(onQuietReady, onQuietFail);
     };
 
