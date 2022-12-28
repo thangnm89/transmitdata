a6365d4773f83518d25d78e783f3c6865b3cb9dc
thangnm89 <106656777+thangnm89@users.noreply.github.com>
2022-12-28 11:31:18 +0700

    update

--- js/receivetext.js
+++ js/receivetext.js
@@ -1,4 +1,4 @@
-var TextReceiver = (function() {
+var TextReceiver = (function () {
     var receivers;
     var result;
 
@@ -7,7 +7,7 @@ var TextReceiver = (function() {
         recvObj.target.textContent = Quiet.ab2str(recvObj.content);
         recvObj.successes++;
         var total = recvObj.failures + recvObj.successes
-        var ratio = recvObj.failures/total * 100;
+        var ratio = recvObj.failures / total * 100;
         recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
     };
 
@@ -21,7 +21,7 @@ var TextReceiver = (function() {
         recvObj.warningbox.classList.remove("hidden");
         recvObj.failures = num_fails;
         var total = recvObj.failures + recvObj.successes
-        var ratio = recvObj.failures/total * 100;
+        var ratio = recvObj.failures / total * 100;
         recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
     };
 
@@ -31,38 +31,40 @@ var TextReceiver = (function() {
         e.target.innerText = e.target.getAttribute('data-quiet-receiving-text');
         e.target.setAttribute('data-quiet-receiving-text', originalText);
 
-        var receiverOnReceive = function(payload) { onReceive(payload, recvObj); };
-        var receiverOnReceiverCreateFail = function(reason) { onReceiverCreateFail(reason, recvObj); };
-        var receiverOnReceiveFail = function(num_fails) { onReceiveFail(num_fails, recvObj); };
-        Quiet.receiver({profile: recvObj.profilename,
+        var receiverOnReceive = function (payload) { onReceive(payload, recvObj); };
+        var receiverOnReceiverCreateFail = function (reason) { onReceiverCreateFail(reason, recvObj); };
+        var receiverOnReceiveFail = function (num_fails) { onReceiveFail(num_fails, recvObj); };
+        Quiet.receiver({
+            profile: recvObj.profilename,
             onReceive: receiverOnReceive,
             onCreateFail: receiverOnReceiverCreateFail,
             onReceiveFail: receiverOnReceiveFail
         });
-        document.getElementById("result").innerHTML  = recvObj.target.textContent;
+
 
         recvObj.target.classList.remove('hidden');
     }
 
-    function setupReceiver(receiver) {
+    function setupReceiver(receiver,result) {
         var recvObj = {
             profilename: receiver.getAttribute('data-quiet-profile-name'),
             btn: receiver.querySelector('[data-quiet-receive-text-button]'),
             target: receiver.querySelector('[data-quiet-receive-text-target]'),
+            target:result,
             warningbox: receiver.querySelector('[data-quiet-receive-text-warning]'),
             successes: 0,
             failures: 0,
             content: new ArrayBuffer(0),
-            
+
         };
-       
-        var onBtnClick = function(e) { return onClick(e, recvObj); };
+
+        var onBtnClick = function (e) { return onClick(e, recvObj); };
         recvObj.btn.addEventListener('click', onBtnClick, false);
     };
 
     function onQuietReady() {
         for (var i = 0; i < receivers.length; i++) {
-            setupReceiver(receivers[i]);
+            setupReceiver(receivers[i],result);
         }
     };
 
@@ -75,7 +77,8 @@ var TextReceiver = (function() {
 
     function onDOMLoad() {
         receivers = document.querySelectorAll('[data-quiet-receive-text]');
-       
+        result = document.querySelector('result');
+
         Quiet.addReadyCallback(onQuietReady, onQuietFail);
     };
 
