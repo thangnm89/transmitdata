b00ff66969623dceab798191be4ad2252f12071c
thangnm89 <106656777+thangnm89@users.noreply.github.com>
2022-12-28 14:14:31 +0700

    h

--- js/receivetext.js
+++ js/receivetext.js
@@ -6,23 +6,23 @@ var TextReceiver = (function() {
     });
     var target;
     var content = new ArrayBuffer(0);
-    var warningbox;
+    // var warningbox;
 
     function onReceive(recvPayload) {
         content = Quiet.mergeab(content, recvPayload);
         target.textContent = Quiet.ab2str(content);
-        warningbox.classList.add("hidden");
+        // warningbox.classList.add("hidden");
     };
 
     function onReceiverCreateFail(reason) {
         console.log("failed to create quiet receiver: " + reason);
-        warningbox.classList.remove("hidden");
-        warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
+        // warningbox.classList.remove("hidden");
+        // warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
     };
 
     function onReceiveFail(num_fails) {
-        warningbox.classList.remove("hidden");
-        warningbox.textContent = "We didn't quite get that. It looks like you tried to transmit something. You may need to move the transmitter closer to the receiver and set the volume to 50%."
+        // warningbox.classList.remove("hidden");
+        // warningbox.textContent = "We didn't quite get that. It looks like you tried to transmit something. You may need to move the transmitter closer to the receiver and set the volume to 50%."
     };
 
     function onQuietReady() {
@@ -36,13 +36,13 @@ var TextReceiver = (function() {
 
     function onQuietFail(reason) {
         console.log("quiet failed to initialize: " + reason);
-        warningbox.classList.remove("hidden");
-        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
+        // warningbox.classList.remove("hidden");
+        // warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
     };
 
     function onDOMLoad() {
         target = document.querySelector('[data-quiet-receive-text-target]');
-        warningbox = document.querySelector('[data-quiet-warning]');
+        // warningbox = document.querySelector('[data-quiet-warning]');
         Quiet.addReadyCallback(onQuietReady, onQuietFail);
     };
 
